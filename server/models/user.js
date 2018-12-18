213e6dad5a6c1d6ae8796a55eea5c9d5f7c8165f
Renan Garcia <bg_renan@hotmail.com>
2018-12-18 16:37:30 -0200

    initial

---
+++ server/models/user.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    photo: String,
+    googleUser_id: String,
+    level: String,
+    score: Number,
+    resources: {words: [{ word: String, meaning: String, howKnown: String}], quote: [{
+      quote: String,
+      source: String
+    }]},
+    
+});
+
+var userModel = mongoose.model('userModel', userSchema);
+
+module.exports = userModel;
